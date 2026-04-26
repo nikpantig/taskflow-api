a0ac8ab521b3c0b30eb9fe0381cc77ebb008a80b
Nikko Pantig <npantig@servingintel.com>
2026-04-26 09:05:42 +0800

    Add initial MVC setup

---
+++ TaskFlow.API/Controllers/TasksViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.API.Controllers
+{
+    public class TasksViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
